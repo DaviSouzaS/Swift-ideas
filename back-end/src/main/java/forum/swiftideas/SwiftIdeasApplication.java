0731086b4040665828af3bbdc6da9c41d95eee77
davi <daavisouza.santana@gmail.com>
2023-10-29 21:26:12 -0300

    create spring boot project

---
+++ back-end/src/main/java/forum/swiftideas/SwiftIdeasApplication.java
@@ -0,0 +1,13 @@
+package forum.swiftideas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftIdeasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftIdeasApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/forum/swiftideas/SwiftIdeasApplicationTests.java
@@ -0,0 +1,13 @@
+package forum.swiftideas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftIdeasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
